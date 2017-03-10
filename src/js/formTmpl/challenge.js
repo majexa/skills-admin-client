43058dc2d36a448144696c4f93e7dab9a8f2aaf2
Andrey-Yartsev <andrey.yartsev.g@gmail.com>
2017-03-10 14:16:58 +0300

    challenge form fixes

--- src/js/formTmpl/challenge.js
+++ src/js/formTmpl/challenge.js
@@ -1,4 +1,7 @@
-Ngn.toObj('Ngn.formTmpl.challenge', <form action="" id="fa61afa93fe367fd5a84279533e84a450" method="post"><div class="element row_skillType type_select name_skillType"><p class="label"><span class="ttl">Тип скила</span><span>:</span></p><div class="field-wrapper">\
+Ngn.toObj('Ngn.formTmpl.challenge', '<div class="apeform"><form action="" id="f3e893d5e8019e286278fc2c464d55231" method="post"><div class="element row_title type_text name_title"><p class="label"><span class="ttl">Название</span><b class="reqStar" title="Required" style="cursor:help">*</b><span>:</span></p><div class="field-wrapper">\
+<input size="40" type="text" id="titlei" maxlength="255" name="title" value="" class="required" />\
+</div><div class="help"></div><div class="clear"></div></div><div class="colSet colN2"><!-- Open fields group depth={0}, type={col}, id={fld2} -->\
+<div class="clear hgrp hgrp_fld2 type_col"><div class="element row_skillType type_select name_skillType"><p class="label"><span class="ttl">Тип скила</span><span>:</span></p><div class="field-wrapper">\
 <select name="skillType" maxlength="255" name="skillType" value="" id="skillTypei">\
 	<option value="power">power</option>\
 	<option value="energy">energy</option>\
@@ -7,24 +10,23 @@ Ngn.toObj('Ngn.formTmpl.challenge', <form action="" id="fa61afa93fe367fd5a842795
 <select name="reportType" maxlength="255" name="reportType" value="" id="reportTypei">\
 	<option value="photo">photo</option>\
 	<option value="text">text</option></select>\
-</div><div class="help"></div><div class="clear"></div></div><div class="element row_title type_text name_title"><p class="label"><span class="ttl">Название</span><b class="reqStar" title="Required" style="cursor:help">*</b><span>:</span></p><div class="field-wrapper">\
-<input size="40" type="text" id="titlei" maxlength="255" name="title" value="" class="required" />\
 </div><div class="help"></div><div class="clear"></div></div><div class="element row_shortDesc type_textarea name_shortDesc" data-name="shortDesc" data-typejs="1"><p class="label"><span class="ttl">Краткое описание</span><span>:</span></p><div class="field-wrapper">\
 <textarea name="shortDesc" maxlength="65000" name="shortDesc" value=""></textarea>\
 </div><div class="help"></div><div class="clear"></div></div><div class="element row_fullDesc type_textarea name_fullDesc" data-name="fullDesc" data-typejs="1"><p class="label"><span class="ttl">Полное описание</span><span>:</span></p><div class="field-wrapper">\
 <textarea name="fullDesc" maxlength="65000" name="fullDesc" value=""></textarea>\
-</div><div class="help"></div><div class="clear"></div></div><div class="element row_price type_price name_price"><p class="label"><span class="ttl">Цена</span><span>:</span></p><div class="field-wrapper">\
+</div><div class="help"></div><div class="clear"></div></div></div><!-- Close fields group depth={0} (open header depth 0) --><!-- Open fields group depth={0}, type={col}, id={fld8} -->\
+<div class="clear hgrp hgrp_fld8 type_col"><div class="element row_price type_price name_price"><p class="label"><span class="ttl">Цена</span><span>:</span></p><div class="field-wrapper">\
 <input size="40" type="text" id="pricei" maxlength="11" name="price" value="123" />\
 </div><div class="help"></div><div class="clear"></div></div><div class="element row_periodType type_select name_periodType"><p class="label"><span class="ttl">Тип периода</span><span>:</span></p><div class="field-wrapper">\
 <select name="periodType" maxlength="255" name="periodType" value="" id="periodTypei">\
 	<option value="default">1 день</option>\
 	<option value="partial">partial</option>\
 	<option value="userStart">userStart</option></select>\
-</div><div class="help"></div><div class="clear"></div></div><div class="clear"></div>\
-<div id="fa61afa93fe367fd5a84279533e84a450js" style="display:none">/u/js/cache/form/fa61afa93fe367fd5a84279533e84a450.js?1488687281</div>\
-<div id="fa61afa93fe367fd5a84279533e84a450jsInline" style="display:none">\
+</div><div class="help"></div><div class="clear"></div></div></div><!-- Close fields group depth={0} (end of elements) --><div class="clear"><!-- --></div></div><div class="clear"></div>\
+<div id="f3e893d5e8019e286278fc2c464d55231js" style="display:none">/u/js/cache/form/f3e893d5e8019e286278fc2c464d55231.js?1489144560</div>\
+<div id="f3e893d5e8019e286278fc2c464d55231jsInline" style="display:none">\
 \
 (function() {\
-  Ngn.Form.forms.fa61afa93fe367fd5a84279533e84a450.initUpload();\
+  Ngn.Form.forms.f3e893d5e8019e286278fc2c464d55231.initUpload();\
 }).delay(100);\
-</div></form>');
+</div></form></div>');
--- src/js/formTmpl/user.js
+++ src/js/formTmpl/user.js
@@ -1,12 +1,12 @@
-Ngn.toObj('Ngn.formTmpl.user', <form action="" id="f0cd547dae68608c5feb3f120ef48a189" method="post"><div class="element row_phone type_text name_phone"><p class="label"><span class="ttl">Телефон</span><span>:</span></p><div class="field-wrapper">\
+Ngn.toObj('Ngn.formTmpl.user', '<div class="apeform"><form action="" id="f0cd547dae68608c5feb3f120ef48a189" method="post"><div class="element row_phone type_text name_phone"><p class="label"><span class="ttl">Телефон</span><span>:</span></p><div class="field-wrapper">\
 <input size="40" type="text" id="phonei" maxlength="255" name="phone" value="" />\
 </div><div class="help"></div><div class="clear"></div></div><div class="element row_deviceToken type_text name_deviceToken"><p class="label"><span class="ttl">Device token</span><span>:</span></p><div class="field-wrapper">\
 <input size="40" type="text" id="deviceTokeni" maxlength="255" name="deviceToken" value="" />\
 </div><div class="help"></div><div class="clear"></div></div><div class="clear"></div>\
-<div id="f0cd547dae68608c5feb3f120ef48a189js" style="display:none">/u/js/cache/form/f0cd547dae68608c5feb3f120ef48a189.js?1489092653</div>\
+<div id="f0cd547dae68608c5feb3f120ef48a189js" style="display:none">/u/js/cache/form/f0cd547dae68608c5feb3f120ef48a189.js?1489143141</div>\
 <div id="f0cd547dae68608c5feb3f120ef48a189jsInline" style="display:none">\
 \
 (function() {\
   Ngn.Form.forms.f0cd547dae68608c5feb3f120ef48a189.initUpload();\
 }).delay(100);\
-</div></form>');
+</div></form></div>');
